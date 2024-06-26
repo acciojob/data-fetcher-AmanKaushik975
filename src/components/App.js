e36753695001a264f7df91d1f33d1ad6765e5515
amank <aman.kaushik.975@gmail.com>
2024-06-26 10:09:25 +0530

    first

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,42 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useEffect, useState } from "react";
+import "./../styles/App.css";
+import axios from "axios";
 
 const App = () => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [msg, setMsg] = useState("loading...");
+
+  function fetchData() {
+    setLoading(true);
+
+    axios
+      .get("https://dummyjson.com/products")
+      .then((response) => {
+        setData(JSON.stringify(response.data, null, 2));
+        setLoading(false);
+        setMsg("loading...");
+      })
+      .catch((err) => {
+        console.log(err);
+        setMsg("An error occurred: ");
+      });
+  }
+  useEffect(() => {
+    fetchData();
+  }, []);
   return (
     <div>
-        {/* Do not remove the main div */}
+      {loading ? (
+        <p>{msg}</p>
+      ) : (
+        <div>
+          <h1>Data Fetched from API</h1>
+          <pre>{data}</pre>
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
